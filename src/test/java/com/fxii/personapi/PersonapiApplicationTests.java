a5d4292f7e004e02507866458e9bd7e3624e8970
Fabricio Kassardjian <fkay13@gmail.com>
2022-01-28 23:25:51 -0300

    commit incial do com projeto gerado pelo spring initializr

---
+++ src/test/java/com/fxii/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxii.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
